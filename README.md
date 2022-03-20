fdcbd79ebcf4b62e250c0d5a818afb2683ea00d7
Dragos Trandafir <dragostrandafir80@gmail.com>
2022-03-20 12:18:23 +0200

    Updated Readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# ✅ Basic Minter DAPP
+
+Made with React by following https://docs.alchemyapi.io/alchemy/tutorials/nft-minter and readjusting it so the user can only connect wallet and mint a collection of NFTs whose URI and price is already assigned beforehand. 
+
+Will style and add more functionality in the near future.
---
+++ src/util/interact.js
@@ -0,0 +1,124 @@
+require('dotenv').config();
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey); 
+const contractABI = require('../contract-abi.json')
+const contractAddress = "0xAF7652142346970Dc257F06C5D05884eA5dE6dfe"; 
+const price = 0.01;
+
+
+export const connectWallet = async () => {
+    if (window.ethereum) {
+      try {
+        const addressArray = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        const obj = {
+          status: "",
+          address: addressArray[0],
+        };
+        return obj;
+      } catch (err) {
+        return {
+          address: "",
+          status: "😥 " + err.message,
+        };
+      }
+    } else {
+      return {
+        address: "",
+        status: (
+          <span>
+            <p>
+              {" "}
+              🦊{" "}
+              <a target="_blank" href={`https://metamask.io/download.html`}>
+                You must install Metamask, a virtual Ethereum wallet, in your
+                browser.
+              </a>
+            </p>
+          </span>
+        ),
+      };
+    }
+  };
+
+  export const mintNFT = async(amount) => {
+     
+    
+    //set up your Ethereum transaction
+    const transactionParameters = {
+        gas: parseInt((258000*amount).toString()).toString(16),
+        to: contractAddress, // Required except during contract publications.
+        from: window.ethereum.selectedAddress, // must match user's active address.
+        value: parseInt(web3.utils.toWei((price * amount).toString(),"ether")).toString(16),
+        data: window.contract.methods.mint(window.ethereum.selectedAddress, amount).encodeABI() //make call to NFT smart contract 
+    };
+
+      
+    //sign transaction via Metamask
+    try {
+        const txHash = await window.ethereum
+            .request({
+                method: 'eth_sendTransaction',
+                params: [transactionParameters],
+            });
+        return {
+            
+            success: true,
+            status: "✅ Check out your transaction on Etherscan: https://rinkeby.etherscan.io/tx/" + txHash
+        }
+    } catch (error) {
+        return {
+            success: false,
+            status: "😥 Something went wrong: " + error.message
+        }
+    }
+}
+
+  export const getCurrentWalletConnected = async () => {
+    //load smart contract
+    window.contract = await new web3.eth.Contract(contractABI, contractAddress);//loadContract();
+    
+    //index = await window.contract.methods.totalSupply().call();
+
+    if (window.ethereum) {
+      try {
+        const addressArray = await window.ethereum.request({
+          method: "eth_accounts",
+        });
+        if (addressArray.length > 0) {
+          return {
+            address: addressArray[0],
+            status: "",
+          };
+        } else {
+          return {
+            address: "",
+            status: "🦊 Connect to Metamask using the top right button.",
+          };
+        }
+      } catch (err) {
+        return {
+          address: "",
+          status: "😥 " + err.message,
+        };
+      }
+    } else {
+      return {
+        address: "",
+        status: (
+          <span>
+            <p>
+              {" "}
+              🦊{" "}
+              <a target="_blank" href={`https://metamask.io/download.html`}>
+                You must install Metamask, a virtual Ethereum wallet, in your
+                browser.
+              </a>
+            </p>
+          </span>
+        ),
+      };
+    }
+  };
